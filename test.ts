2d751fb323ecaad75f87668bfc6ea1a1e0084c27
mayankverma786 <mayankverma786@gmail.com>
2019-10-16 03:22:12 +0530

    Initial Commit

---
+++ test.ts
@@ -0,0 +1,12 @@
+function sayHello() {
+    //return `Say Hello to My Little Friend`    
+    var b = '123';
+    return b;
+}
+
+function newfunc(){
+    var a = sayHello();
+    console.log(a);
+}
+
+newfunc();
